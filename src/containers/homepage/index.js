efb7ffa64fbb91c216103c0bc17f9d89fc06e01e
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2019-01-27 17:06:54 -0500

    Organizing project architecture. Fixed #5.

---
+++ src/containers/homepage/index.js
@@ -0,0 +1,13 @@
+// @vendors
+import React from 'react';
+
+// @styling
+import './styles.css';
+
+const Homepage = () => (
+    <div className="homepage">
+        Homepage
+    </div>
+);
+
+export default Homepage;
---
+++ src/containers/login/index.js
@@ -0,0 +1,13 @@
+// @vendors  
+import React from 'react';
+
+// @styling
+import './styles.css';
+
+const Login = () => (
+    <div className="login">
+        Login
+    </div>
+);
+
+export default Login;
