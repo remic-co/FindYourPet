55895048e7ce8654b8f251305271edf49457ace4
Sara Guzmán <saruchis34@gmail.com>
2019-02-04 18:52:55 -0500

    create email input

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -7,9 +7,29 @@ import './styles.css';
 const Login = () => (
     <div className="login">
         <div className="container">
+<<<<<<< HEAD
             Container
+=======
+
+               <p className="tittle">Login</p>
+                <br></br>
+ 
+              
+               <p className="t-e"> e-mail </p>
+               <div className="cont-t-e">
+               <input className="e-mail" type="text"></input>
+               </div>
+               <br></br>
+              
+
+              <p className="t-p">password</p> 
+              <input className="password" type="password"></input>
+
+>>>>>>> 469e3d3... create email input
         </div>
     </div>
+
+  
 );
 
 export default Login;
